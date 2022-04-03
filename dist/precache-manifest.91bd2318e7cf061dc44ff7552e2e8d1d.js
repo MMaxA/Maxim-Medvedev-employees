51c0c2cc68393a8fa56624cc34bf31cabd4d9f7a
MMaxA <Medvedevmaa@gmail.com>
2022-04-03 20:05:24 +0500

    Add files via upload

---
+++ dist/precache-manifest.91bd2318e7cf061dc44ff7552e2e8d1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "015d761fa640b6900c3ca88476c5b8de",
+    "url": "/MMaxA--Maxim-Medvedev-employees/index.html"
+  },
+  {
+    "revision": "f238bd67b21893d213ed",
+    "url": "/MMaxA--Maxim-Medvedev-employees/js/app.35643b31.js"
+  },
+  {
+    "revision": "7b1bcbbc5445e8d3030b",
+    "url": "/MMaxA--Maxim-Medvedev-employees/js/chunk-vendors.2d224698.js"
+  },
+  {
+    "revision": "a30f9aee2ce34c12c98d9087c3d2fd04",
+    "url": "/MMaxA--Maxim-Medvedev-employees/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/MMaxA--Maxim-Medvedev-employees/robots.txt"
+  }
+]);
